d6e0f524f3f3af60aa380beb7e8f87c1e10a0163
Nachanon Montikanon <nachanon.montikanon@mail.kmutt.ac.th>
2020-04-18 11:19:44 +0700

    [UPDATE] Employee
    เพิ่ม method พื้นฐาน กำลังคิดว่า employee สามารถทำการอะไรได้บ้างกับ bts

--- src/memberSystem/Employee.java
+++ src/memberSystem/Employee.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package memberSystem;
 
-/**
- *
- * @author Gigabyte
- */
 public class Employee {
+
+    private long employee_id;
+    private String password;
+    private Person person;
+
+    public Employee(Employee employee) {
+        this.employee_id = employee.employee_id;
+        this.password = employee.password;
+    }
+
+    public Employee(long employee_id, Person person) {
+        this.employee_id = employee_id;
+        this.person = person;
+    }
+
+    public long getEmployee_id() {
+        return employee_id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean resetPassword() {
+        this.password = null;
+        return true;
+    }
+    
     
 }
