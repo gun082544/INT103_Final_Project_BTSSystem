c817a403ece10c20ca2ebaab81bc3b5efc74d398
Nachanon Montikanon <nachanon.montikanon@mail.kmutt.ac.th>
2020-04-18 10:45:47 +0700

    Add Person,Employee class
    ทดลองสร้างคลาส

---
+++ src/memberSystem/Person.java
@@ -0,0 +1,10 @@
+
+package memberSystem;
+
+public class Person {
+        private String firstname;
+        private String lastname;
+        private int c_id; //citizen ID
+        private int phone;
+}
+
