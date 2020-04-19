2b837ea87988d144606a6e39196c206b0bd41ace
Gun <gun082544@hotmail.com>
2020-04-19 16:16:14 +0700

    [ADD] RabbitCard

--- src/memberSystem/RabbitCard.java
+++ src/memberSystem/RabbitCard.java
@@ -1,14 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package memberSystem;
 
-/**
- *
- * @author PUNNAPOP
- */
+
 public class RabbitCard {
+    
+    private long idCard;
+    private Person person;
+    private int money;
+    private int point;
+    private CardStatus cardStatus;
+    private CardType cardType;
+
+    public RabbitCard(long idCard, Person person) {
+        this.idCard = idCard;
+        this.person = person;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public long getIdCard() {
+        return idCard;
+    }
+
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+
+    public CardStatus getCardStatus() {
+        return cardStatus;
+    }
+
+    public void setCardStatus(CardStatus cardStatus) {
+        this.cardStatus = cardStatus;
+    }
+
+    public CardType getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(CardType cardType) {
+        this.cardType = cardType;
+    }
+
+    public RabbitCard(long idCard, Person person, int money, int point, CardStatus cardStatus, CardType cardType) {
+        this.idCard = idCard;
+        this.person = person;
+        this.money = money;
+        this.point = point;
+        this.cardStatus = cardStatus;
+        this.cardType = cardType;
+    }
+    
     
 }
