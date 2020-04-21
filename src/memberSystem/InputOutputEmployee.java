6082ae31fe43f1fc36353507e3a68bca20577acf
Nachanon Montikanon <nachanon.montikanon@mail.kmutt.ac.th>
2020-04-21 23:48:06 +0700

    [UPDATE] ยังคงพัง อย่าพึ่งเอาไปใช้
    พังอย่างต่อเนื่องกับ input/output

--- src/memberSystem/InputOutputEmployee.java
+++ src/memberSystem/InputOutputEmployee.java
@@ -1,5 +1,7 @@
 package memberSystem;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -9,40 +11,48 @@ import java.io.ObjectOutputStream;
 
 public class InputOutputEmployee {
 
-    static String path = "INT103_Final_Project_BTSSystem\\tmp";
-    static String filename = "Employee.txt";
-    static String absolutePath = path + filename;
+    static String path = "tmp/Employee.txt";
+      
+      public static void WriteEmployee(Employee em){
+         try (FileOutputStream fos = new FileOutputStream("tmp/Employee"+".txt");
+                BufferedOutputStream bos=new BufferedOutputStream(fos);
+                DataOutputStream dos = new DataOutputStream(bos)) {
+            String textfromfile = "";
+            while (em.)
 
-    public static void WriteEmployee(Employee em) {
-        try ( FileOutputStream fos = new FileOutputStream(absolutePath, false);  
-                ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            oos.writeObject(em + "\n");
-            oos.close();
-            fos.close();
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-    }
+      }
+//    public static void WriteEmployee(Employee em) {
+//        try ( FileOutputStream fos = new FileOutputStream(path);  ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+//            oos.writeObject(em + "\n");
+//            oos.close();
+//            fos.close();
+//        } catch (FileNotFoundException ex) {
+//            ex.printStackTrace();
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//    }
 
-    public static Employee ReadEmployee() {
-        Employee em = null;
-        try (FileInputStream fis = new FileInputStream(absolutePath);
-                ObjectInputStream ois = new ObjectInputStream(fis)) {
-            
-            while(fis.available()!=0){
-               em = (Employee) ois.readObject();
-            }
-           
-        }catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        }catch (IOException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-           ex.printStackTrace();
-        }
-        return em;
-    }
+//    public static Employee ReadEmployee() {
+//        Employee em = null;
+//        try ( FileInputStream fis = new FileInputStream(path);  ObjectInputStream ois = new ObjectInputStream(fis)) {
+//
+//            while (fis.available() != 0) {
+//                em = (Employee) ois.readObject();
+//            }
+//
+//        } catch (FileNotFoundException ex) {
+//            ex.printStackTrace();
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        } catch (ClassNotFoundException ex) {
+//            ex.printStackTrace();
+//        }
+//        return em;
+//    }
 }
-
