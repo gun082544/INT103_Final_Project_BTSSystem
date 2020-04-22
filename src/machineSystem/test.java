abcd454d4f21d0cfe210d2de4b5e3a636b73ead2
UntillNIGHT <56515120+lengleng9090@users.noreply.github.com>
2020-04-23 00:01:48 +0700

    [update]test

--- src/machineSystem/test.java
+++ src/machineSystem/test.java
@@ -10,19 +10,22 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import memberSystem.CardStatus;
+import memberSystem.CardType;
 import memberSystem.Person;
 import memberSystem.RabbitCard;
+import dataBase.RabbitcardDatabase;
 
 public class test {
     public void writeRabbitCardLog(RabbitCard r1, String atIn, String atOut) throws IOException {
         /////// catch for FileNotFound and creat new txt file ///////
         try{
-            FileInputStream fis = new FileInputStream("tmp/" + r1.getIdCard() + "_Station_Log.txt");
+            FileInputStream fis = new FileInputStream("tmp/" + r1.getRbc_idCard() + "_Station_Log.txt");
         }catch(FileNotFoundException ex){
-            FileWriter S1 = new FileWriter("tmp/" + r1.getIdCard() + "_Station_Log.txt");
+            FileWriter S1 = new FileWriter("tmp/" + r1.getRbc_idCard() + "_Station_Log.txt");
         }
         
-        Path file = Paths.get("tmp/" + r1.getIdCard() + "_Station_Log.txt");
+        Path file = Paths.get("tmp/" + r1.getRbc_idCard() + "_Station_Log.txt");
         /////// backup file to temp ///////
         BufferedReader reader = Files.newBufferedReader(file, StandardCharsets.UTF_8);
         String line = null;
@@ -44,7 +47,9 @@ public class test {
 
     }
     public static void main(String[] args) throws IOException {
-        RabbitCard r1 = new RabbitCard(1001, new Person("Somchai", "Somtam", "1102170018970", "0806849641"));
+        RabbitCard r1 = new RabbitCard(1001, new Person("Somchai", "Somtam", "1102170018970", "0806849641"),100, 100, CardStatus.ACTIVE, CardType.ADULT);
+        RabbitcardDatabase DBR1 = new RabbitcardDatabase();
+        DBR1.insertDB(0, new Person("Somchai", "Somtam", "1102170018970", "0806849641"), 0, 0, CardStatus.ACTIVE, CardType.ADULT);
         test mac1 = new test();
         mac1.writeRabbitCardLog(r1, "Siam", "leng house");
     }
