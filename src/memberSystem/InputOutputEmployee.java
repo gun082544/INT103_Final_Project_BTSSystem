753e78104a325ac618ae619ebcba5be6a2a427ce
Nachanon Montikanon <nachanon.montikanon@mail.kmutt.ac.th>
2020-04-25 14:09:00 +0700

    [UPDATE] Write Read
    เรียบร้อย ติดแค่แม่งไม่ยอมข้ามของเก่าให้ ทำให้เวลาเขียนมันเขียนทับของเก่า เดะค่อยแก้

--- src/memberSystem/InputOutputEmployee.java
+++ src/memberSystem/InputOutputEmployee.java
@@ -2,10 +2,12 @@ package memberSystem;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,56 +15,58 @@ import java.nio.file.Paths;
 
 public class InputOutputEmployee {
 
-    static String path = "tmp/Employee.txt";
+    static File file = new File("C:\\Users\\Gigabyte\\Documents\\GitHub\\INT103_Final_Project_BTSSystem\\tmp\\Employee.txt");
 
-    public static void WriteEmployee(Employee em) throws IOException {
+    public static void WriteEmployee(Employee e) throws IOException {
         try {
-            FileInputStream fis = new FileInputStream(path);
-        } catch (FileNotFoundException e) {
-            FileWriter fw = new FileWriter(path);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            Path file = Paths.get("C:\\Users\\Gigabyte\\Documents\\GitHub\\INT103_Final_Project_BTSSystem\\tmp\\Employee.txt");
+            BufferedReader bf = Files.newBufferedReader(file, Charset.defaultCharset());
+            String line = null ;
+            StringBuilder tempt = new StringBuilder();
+            while ((line = bf.readLine()) == null) {
+                    tempt.append(line +"\n");
+            }
+            BufferedWriter writer = new BufferedWriter(fw);
+            StringBuilder result = new StringBuilder();
+            result.append(e.toString() + "\n");
+            result.append("---------------------------------------------------------------\n");
+            writer.write(tempt.toString());
+            writer.write(result.toString());
+            writer.close();
+        } catch (IOException i) {
+            i.printStackTrace();
         }
-        Path file = Paths.get(path);
-        BufferedReader bf = Files.newBufferedReader(file, StandardCharsets.UTF_8);
-        String line = null;
-        StringBuilder temp = new StringBuilder();
-        while ((line = bf.readLine()) != null) {
-            temp.append(line + "\n");
-        }
-        BufferedWriter writer = Files.newBufferedWriter(file, StandardCharsets.UTF_8);
-        StringBuilder result = new StringBuilder();
-        result.append("Employees's Data\n");
-        result.append(em.toString() + "\n");
-        result.append("------------------------------\n");
-        writer.write(temp.toString());
-        writer.write(result.toString());
-        writer.close();
     }
 
     public static void ReadEmployee() throws IOException {
         try {
-            FileInputStream fis = new FileInputStream(path);
-            Path file = Paths.get(path);
-            BufferedReader bf = Files.newBufferedReader(file, StandardCharsets.UTF_8);
+            Path file = Paths.get("C:\\Users\\Gigabyte\\Documents\\GitHub\\INT103_Final_Project_BTSSystem\\tmp\\Employee.txt");
+            BufferedReader bf = Files.newBufferedReader(file, Charset.defaultCharset());
             String line = null;
             StringBuilder temp = new StringBuilder();
             while ((line = bf.readLine()) != null) {
                 temp.append(line + "\n");
             }
             System.out.println(temp.toString());
-        } catch (FileNotFoundException e) {
-            System.out.println("-----------No Employee Data-----------");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    public static void main(String[] args) throws IOException{
-       Person p1 = new Person("JJ","Montika","62130500024","086849641");
-       Person p2 = new Person("Leng","Loha","62130500056","086148152");
-       Person p3 = new Person("Gun","Chalor","62130500072","092151852");
-       
-       Employee e1 = new Employee("A01","btsemp",p1);
-       Employee e2 = new Employee("A02","btsemp",p2);
-       Employee e3 = new Employee("A03","btsemp",p3);
-       
-       
-       ReadEmployee();
+
+    public static void main(String[] args) throws IOException {
+        Person p1 = new Person("JJ", "Montika", "62130500024", "086849641");
+        Person p2 = new Person("Leng", "Loha", "62130500056", "086148152");
+        Person p3 = new Person("Gun", "Chalor", "62130500072", "092151852");
+
+        Employee e1 = new Employee("A01", "btsemp", p1);
+        Employee e2 = new Employee("A02", "btsemp", p2);
+        Employee e3 = new Employee("A03", "btsemp", p3);
+        
+        WriteEmployee(e1);
+
     }
 }
