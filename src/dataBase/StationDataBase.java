8ae266788f2632b0fdf6b9c4d0d569d4a770d6cc
UntillNIGHT <56515120+lengleng9090@users.noreply.github.com>
2020-04-26 14:37:01 +0700

    [fix]Station DB

---
+++ src/dataBase/StationDataBase.java
@@ -0,0 +1,67 @@
+package dataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StationDataBase {
+
+    public void insertDB(int sta_id, String Sta_name, int sta_distance) {
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://103.86.49.133:3306/BTSMachineSystem", "KondlnwSoftwarehouse", "Kodlnw1234");
+                Statement stm = conn.createStatement();) {
+            
+            int row = stm.executeUpdate("INSERT INTO STATION VALUES(" + sta_id + ",'" + Sta_name + "'," + sta_distance + ")");
+            
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("ERROR : This method isn't complete.");
+        }
+    }
+
+    public String[] SelectNameFromSTATION() {
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://103.86.49.133:3306/BTSMachineSystem", "KondlnwSoftwarehouse", "Kodlnw1234");
+                Statement stm = conn.createStatement();) {
+            int count = 0, i = 0;
+            ////////////////////////
+            ResultSet stationNameCount = stm.executeQuery("SELECT * FROM STATION");
+            while (stationNameCount.next()) {
+                count++;
+            }
+            String Station_name[] = new String[count];
+            ////////////////////////
+            ResultSet stationName = stm.executeQuery("SELECT * FROM STATION");
+            while (stationName.next()) {
+                Station_name[i++] = stationName.getString("sta_name");
+            }
+            return Station_name;
+            ////////////////////////
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+        public int[] SelectDistanceFromSTATION() {
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://103.86.49.133:3306/BTSMachineSystem", "KondlnwSoftwarehouse", "Kodlnw1234");
+                Statement stm = conn.createStatement();) {
+            int count = 0, i = 0;
+            ////////////////////////
+            ResultSet stationNameCount = stm.executeQuery("SELECT * FROM STATION");
+            while (stationNameCount.next()) {
+                count++;
+            }
+            int Station_distance[] = new int[count];
+            ////////////////////////
+            ResultSet stationName = stm.executeQuery("SELECT * FROM STATION");
+            while (stationName.next()) {
+                Station_distance[i++] = stationName.getInt("sta_distance");
+            }
+            return Station_distance;
+            ////////////////////////
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+}
